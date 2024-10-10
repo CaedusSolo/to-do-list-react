9bbb123628be3ed0ae281dd76109313777c5ae5c
Ying Tong <yingtong0106@gmail.com>
2024-10-10 13:29:48 +0800

    Added functionality to Add Task button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import MainSection from "./components/MainSection";
 import AddRemoveBtn from "./components/AddRemoveBtn";
-import { nanoid } from "nanoid";
+
 
 export const AppContext = createContext();
 
@@ -13,11 +13,28 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('items', JSON.stringify(listItems))
+    setShowModal(false)
   },[listItems])
 
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
 
+
+  function addTask(categoryName, newTask) {
+    setListItems(prevItems => {
+      return prevItems.map(item => {
+        if (item.category === categoryName) {
+          return {
+            ...item,
+            tasks: [...item.tasks, newTask]
+          }
+        }
+        else {
+          return item
+        }
+      })
+    })
+  }
 
   return (
     <div className="container">
@@ -29,7 +46,8 @@ function App() {
           showModal,
           setShowModal,
           handleShow,
-          handleClose
+          handleClose,
+          addTask
         }}
       >
         <MainSection />
--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -1,14 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../App";
+import { nanoid } from "nanoid";
 
 function AddTaskModal() {
-  const { showModal, setShowModal, handleShow, handleClose, listItems } =
-    useContext(AppContext);
+  const { handleClose, listItems, addTask } = useContext(AppContext);
+
+  const [categorySelected, setCategorySelected] = useState(listItems[0].category);
+  const [newTask, setNewTask] = useState('');  // Initialize as an empty string
 
   function mapOptionValues() {
     return listItems.map((item) => {
-      return <option value={item.category}>{item.category}</option>;
+      return <option key={item.category} value={item.category}>{item.category}</option>;
     });
+  }
+
+  function updateCategorySelected(categorySelected) {
+    setCategorySelected(categorySelected);
+  }
+
+  function updateNewTask(taskName) {
+    setNewTask(taskName);  
+  }
+
+  function handleSaveTask() {
+    const taskObject = {
+      id: nanoid(),
+      task: newTask,
+      completed: false,
+    };
+    addTask(categorySelected, taskObject);  
   }
 
   return (
@@ -30,24 +50,40 @@ function AddTaskModal() {
           <div className="modal-body">
             <div className="categorySelection">
               <label htmlFor="category">Select Category</label>
-              <select name="category" id="category">
+              <select
+                name="category"
+                id="category"
+                value={categorySelected}
+                onChange={(e) => updateCategorySelected(e.target.value)}
+              >
                 {mapOptionValues()}
               </select>
             </div>
             <div className="taskCreation">
               <label htmlFor="newTask">Enter Task Name</label>
-              <input type="text" name="newTask" id="newTask" />
+              <input
+                type="text"
+                name="newTask"
+                id="newTask"
+                value={newTask}
+                onChange={(e) => updateNewTask(e.target.value)}
+              />
             </div>
           </div>
           <div className="modal-footer">
             <button
               type="button"
-              className="btn btn-secondary"
+              className="btn btn-danger"
               onClick={handleClose}
             >
               Close
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              type="button"
+              className="btn btn-primary"
+              disabled={newTask.length < 4}  
+              onClick={handleSaveTask}
+            >
               Save changes
             </button>
           </div>
